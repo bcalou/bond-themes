604162ec1eed8cb29e5d4ad1af7d88f0ba27cba9
bcalou <bastien.calou@gmail.com>
2021-06-22 18:55:49 +0200

    [liquid] add next/previous track links

--- src/11ty/collections.js
+++ src/11ty/collections.js
@@ -4,5 +4,10 @@ module.exports = {
       .getFilteredByGlob('src/movies/*.md')
       .sort((a, b) => a.data.year - b.data.year),
 
-  tracks: (collection) => collection.getFilteredByGlob('src/tracks/*.md'),
+  tracks: (collection) =>
+    collection.getFilteredByGlob('src/tracks/*.md').sort((a, b) => {
+      if (a.data.track > b.data.track) return 1;
+      if (a.data.track < b.data.track) return -1;
+      return 0;
+    }),
 };
